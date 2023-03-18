4178b7e2d7325b44b046f6dcf1ec19e0641e0ad7
E-Vivanco <vivanco.elsa@gmail.com>
2023-03-18 17:41:54 +0000

    ajuste 2°

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template
+import mysql.connector
 from flask_migrate import Migrate
 from models import db
 
@@ -7,10 +8,11 @@ app.url_map.slashes=False
 app.config['DEBUG']=True
 app.config['ENV']='development'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///baseflask.db"
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///basedatosflask2.db"
+
 
 db.init_app(app)
-Migrate(db,app)
+Migrate(app,db) # db init, db migrate, db upgrade
 
 @app.route("/")
 def home():
